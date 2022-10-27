21a5dd3576a3a5e1fd63a23971646d71d4b48b38
Maxime Braud <85800785+KodiakFR@users.noreply.github.com>
2022-10-27 10:33:36 +0200

    add a padding in the navigator page

--- lib/screens/navigator_page.dart
+++ lib/screens/navigator_page.dart
@@ -11,30 +11,33 @@ class NavigatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Navigation TP ENI',style: Theme.of(context).textTheme.headline4,textAlign: TextAlign.center,),
-          const SizedBox(height: 30,),
-          ElevatedButton(
-              onPressed: (() => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Tp1()))),
-              child: const Text('TP 01')),
-               ElevatedButton(
-              onPressed: (() => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Tp2()))),
-              child: const Text('TP 02')),
-               ElevatedButton(
-              onPressed: (() => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Tp3()))),
-              child: const Text('TP 03 + TP 04')),
-               ElevatedButton(
-              onPressed: (() => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Tp5()))),
-              child: const Text('TP 05'))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Navigation TP ENI',style: Theme.of(context).textTheme.headline4,textAlign: TextAlign.center,),
+            const SizedBox(height: 30,),
+            ElevatedButton(
+                onPressed: (() => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Tp1()))),
+                child: const Text('TP 01')),
+                 ElevatedButton(
+                onPressed: (() => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Tp2()))),
+                child: const Text('TP 02')),
+                 ElevatedButton(
+                onPressed: (() => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Tp3()))),
+                child: const Text('TP 03 + TP 04')),
+                 ElevatedButton(
+                onPressed: (() => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Tp5()))),
+                child: const Text('TP 05'))
+          ],
+        ),
       ),
     );
   }
